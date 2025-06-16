c14e614af72def2aa015fb8890a623cebc88ad53
cypriankanda <cyprianboit@gmail.com>
2025-06-16 16:22:48 +0300

    added blueprint for GET

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 # app.py
 from flask import Flask
 from flask_migrate import Migrate
-from models import db  # ✅ import the db instance
-import models  # ✅ import models to register them
+from models import db
+from routes import api
 
 def create_app():
     app = Flask(__name__)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///superheroes.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///superheroes.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     Migrate(app, db)
+
+    app.register_blueprint(api, url_prefix="/")
 
     return app
