40ef36542cdd70e900ab3032287e0499b4eb01fd
cypriankanda <cyprianboit@gmail.com>
2025-06-16 16:25:49 +0300

    Final models.py (with to_dict() added

--- models.py
+++ models.py
@@ -10,6 +10,14 @@ class Hero(db.Model):
     super_name = db.Column(db.String)
     hero_powers = db.relationship('HeroPower', backref='hero', cascade='all, delete-orphan')
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "super_name": self.super_name,
+            "hero_powers": [hp.to_dict() for hp in self.hero_powers]
+        }
+
 class Power(db.Model):
     __tablename__ = 'powers'
     id = db.Column(db.Integer, primary_key=True)
@@ -17,9 +25,25 @@ class Power(db.Model):
     description = db.Column(db.String)
     hero_powers = db.relationship('HeroPower', backref='power', cascade='all, delete-orphan')
 
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
+
 class HeroPower(db.Model):
     __tablename__ = 'hero_powers'
     id = db.Column(db.Integer, primary_key=True)
     strength = db.Column(db.String)
     hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
     power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "hero_id": self.hero_id,
+            "power_id": self.power_id,
+            "strength": self.strength,
+            "power": self.power.to_dict()
+        }
